504fc604e6d3bfe0d225cc1a747ab4f873afe497
Ivan Bernatović <ivan.bernatovic.93@gmail.com>
2015-09-10 07:33:08 +0200

    Finished

--- app/Listeners/EmailInvoice.php
+++ app/Listeners/EmailInvoice.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\OrderWasPlaced;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Mail;
 
 class EmailInvoice
 {
--- app/Listeners/UserRegistrationConfirmation.php
+++ app/Listeners/UserRegistrationConfirmation.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\UserIsRegistered;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Mail;
 
 class UserRegistrationConfirmation
 {
